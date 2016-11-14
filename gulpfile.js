f1ce8ba2cf4a9e6cd2df075984f03aacb0f6e923
nicholasruggeri <ruggerinicholas@gmail.com>
2016-11-14 14:28:45 +0100

    fix position assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,75 +3,38 @@ var gulp = require('gulp'),
     prefix = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    iife = require('gulp-iife'),
-    minifyCss = require('gulp-minify-css');
+    uglify = require('gulp-uglify');
 
-
-
-
-/**
- * Copy files from assets/img into web/public/img
- */
-gulp.task('img', function() {
-    return gulp.src(['assets/img/**/*'])
-        .pipe(gulp.dest('web/public/img'));
-});
-
-/**
- * Copy files from assets/img into web/public/img
- */
-gulp.task('img:dummy', function() {
-    return gulp.src(['assets/dummy-img/**/*'])
-        .pipe(gulp.dest('web/public/dummy-img'));
-});
 
 /**
  * Compile files from assets/scss into web/public/css
  */
 gulp.task('sass', function () {
-    return gulp.src('assets/scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss']
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(minifyCss())
-        .pipe(gulp.dest('web/public/css'));
+    return gulp.src('_assets/scss/main.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(prefix())
+        .pipe(gulp.dest('web/public/assets/css'));
 });
 
 /**
 * Compile files from assets/js into web/public/js
  */
 gulp.task('js', function(){
-    return gulp.src('assets/js/**/*.js')
-        .pipe(concat('main.min.js'))
-        .pipe(iife())
-        // .pipe(uglify())
-        .pipe(gulp.dest('web/public/js'));
+    return gulp.src('_assets/js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('web/public/assets/js'));
 });
 
 /**
-* Compile files from assets/vendor into web/public/js
- */
-gulp.task('js:vendor', function(){
-    return gulp.src([
-            'assets/vendor/angular/angular.min.js',
-            'assets/vendor/salvattore/dist/salvattore.min.js'
-        ])
-        .pipe(concat('vendor.min.js'))
-        //.pipe(uglify())
-        .pipe(gulp.dest('web/public/js'));
-});
-
-/**
- * Watch files, run jekyll & reload BrowserSync
+ * Watch files, run & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/js/**/*.js', ['js']);
+    gulp.watch('_assets/scss/**/*.scss', ['sass']);
+    gulp.watch('_assets/js/**/*.js', ['js']);
 });
 
 /**
- * Default task, running just `gulp` will compile the assets and compile the jekyll site
+ * Default task, running just `gulp` will compile the assets and compile the site
  */
-gulp.task('default', ['sass', 'js' , 'js:vendor', 'img', 'img:dummy']);
+gulp.task('default', ['sass', 'js']);
